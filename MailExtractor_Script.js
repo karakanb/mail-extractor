0bc0e870aa3c81032d03121a9dcb014bc64f16b7
Burak Karakan <burak.karakan@boosmart.com>
2016-01-22 00:52:54 +0200

    Name and code update

--- MailExtractor_Script.js
+++ MailExtractor_Script.js
@@ -1,6 +1,6 @@
-# Just a simple REGEX operation with JavaScript, this functions returns an array of e-mail addresses that the text contains.
+//Just a simple REGEX operation with JavaScript, this functions returns an array of e-mail addresses that the text contains.
 
-	function extractEmails (text)
-	{
-	    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-	}
+function extractEmails (text)
+{
+    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+}
